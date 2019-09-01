ffc7948b0f1aee2c5f76e5c73611cffcbe8f4e9c
pcardona34 <pcardona34@gmail.com>
2019-09-01 17:40:05 +0200

    Première publication

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Jeu du pendu
 
-Version française du jeu du pendu conçu initialement par Dustin Brown et Steven Ferguson.
++ Version française du jeu du pendu conçu initialement par Dustin Brown et Steven Ferguson.
+
+## Autres adaptations
+
++ Séparation de la liste de mots dans le fichier `data/mots.js`.
+
+## Utilisation
+
++ Pour adapter le jeu à votre convenance, éditez les listes de mots dans le fichier `data/mots.js`.
---
+++ data/mots.js
@@ -0,0 +1,10 @@
+// Les niveaux sont suivis de deux points, par exemple "1" :
+// Une liste de mots dans le niveau concerné est placée entre crochets [] : ["motun", "motdeux", "etc"] et suivie d'une virgule ',' sauf pour la dernière liste
+// N'écrivez pas les accents.
+// N'oubliez pas les guillemets.
+
+var wordLists = { 
+  "1" : ["hippopotame","girafe", "gnou"],
+  "2": ["biere", "vin", "eau"],
+  "3": ["tarte", "clafoutis", "fondant"]
+};
